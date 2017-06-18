a5efff3c53a656aff68597cc29bea6db92c9728e
noahbabilone <noahbabilone@gmail.com>
2017-06-18 22:11:35 +0200

    updated .gitignore

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -25,8 +25,8 @@ class DefaultController extends Controller
         if (($handle = fopen($filepath, 'r')) !== false) {
             if ($handle) {
                 while (($line = fgets($handle)) !== false) {
-                    $logs  = explode("\r", $line);
-//                    $logs[] = $logs;
+//                    $logs  = explode("\r", $line);
+                    $logs[] = $logs;
                 }
                 if (!feof($handle)) {
                     $this->setFlash("custom-alerts alert alert-danger fade in",
